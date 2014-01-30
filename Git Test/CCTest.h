8726dc9d2cef6410bd40bf7b3c55e5c45002007d
Chris Clough <chrisclough@Chriss-MacBook-Pro.local>
2014-01-30 12:36:21 -0500

    added test  class

---
+++ Git Test/CCTest.h
@@ -0,0 +1,13 @@
+//
+//  CCTest.h
+//  Git Test
+//
+//  Created by Chris Clough on 1/30/14.
+//  Copyright (c) 2014 Chris Clough. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CCTest : NSObject
+
+@end
---
+++ Git Test/CCTest.m
@@ -0,0 +1,13 @@
+//
+//  CCTest.m
+//  Git Test
+//
+//  Created by Chris Clough on 1/30/14.
+//  Copyright (c) 2014 Chris Clough. All rights reserved.
+//
+
+#import "CCTest.h"
+
+@implementation CCTest
+
+@end
